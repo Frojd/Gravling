bcca53e4653e888150870ccbf3db99dcd5b34f98
Martin Sandström <martin@marteinn.se>
2020-11-26 20:13:21 +0100

    Rename crawler and add support for markup search

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Keyword Detector
+
 This is a tool that helps you identify strings on a website and build reports.
 
 ## Requirements
+
 - python 3.6+
 - pip
 
@@ -13,13 +15,13 @@ pip install -r requirements.txt
 
 ## Usage
 
-### Identifing keywords on a website
+### Gather urls for pages that contains a certain text by crawling sitemap
 
-This command will scrape all pages in the sitemap and return the text 
+This command will scrape all pages in the sitemap and return the matching text and the url where text was found. By default it will only search the human readable text.
 
 ```bash
 cd keyword_detector
-scrapy crawl webcrawler -t csv -o matches.csv -a sitemap_url=https://client.test/sitemap.xml -a keywords="Lynx"
+scrapy crawl sitemap -o matches.csv -a sitemap_url=https://client.test/sitemap.xml -a keywords="Lynx"
 ```
 
 Will generate a list that looks like this:
@@ -30,5 +32,23 @@ Tomorrow. Lynx browser is,https://example.com/about
  is easy. Lynx is quick a,https://example.com/installation
 ```
 
+### Gather urls for pages that has markup that contains a certain text, by crawling sitemap
+
+This command will only search page source markup and return any matches it finds.
+
+```bash
+cd keyword_detector
+scrapy crawl sitemap -o matches.csv -a sitemap_url=https://client.test/sitemap.xml -a keywords="Lynx" -a search_html=1 -a search_text=0
+```
+
+Will generate a list that looks like this:
+
+```csv
+text,url
+orrow. <b>Lynx</b> brows,https://example.com/credits
+. <strong>Lynx</strong> ,https://example.com/unix
+```
+
 ## Security
+
 If you believe you have found a security issue with any of our projects please email us at [security@frojd.se](security@frojd.se).
---
+++ keyword_detector/spiders/keyword_detector_sitemap_spider.py
@@ -0,0 +1,75 @@
+import re
+from io import StringIO
+from functools import partial
+
+from bs4 import BeautifulSoup
+import scrapy
+from scrapy.http import Request
+from scrapy.spiders import SitemapSpider, Rule
+from scrapy.item import Item
+
+from keyword_detector import items
+
+
+class KeywordDetectorSitemapSpider(SitemapSpider):
+    name = "sitemap"
+
+    def __init__(self, sitemap_url=None, keywords=None, search_text="1", search_html="0"):
+        super().__init__()
+
+        print(keywords)
+
+        self.sitemap_urls = [sitemap_url]
+        self.keywords = keywords.split()
+
+        self.search_text = search_text == "1"
+        self.search_html= search_html == "1"
+
+    def parse(self, response):
+        self.logger.info(f"Scraping url {response.url}")
+        matches = []
+
+        if self.search_text:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            document_plain_text = soup.get_text()
+
+
+            for keyword in self.keywords:
+                keyword_matches = extract_terms_and_context_from_text(
+                    keyword, document_plain_text
+                )
+                matches = [
+                    *matches,
+                    *keyword_matches,
+                ]
+
+        if self.search_html:
+            html_text = response.text
+
+            for keyword in self.keywords:
+                keyword_matches = extract_terms_and_context_from_text(
+                    keyword, html_text
+                )
+                matches = [
+                    *matches,
+                    *keyword_matches,
+                ]
+
+        for match in matches:
+            match = match.replace('\n','')
+            yield items.KeywordDetectorItem(text=match, url=response.url)
+
+
+def extract_terms_and_context_from_text(needle, text):
+    text_len = len(text)
+
+    matches = []
+    for m in re.finditer(needle, text):
+        start = m.start()-10
+        start = start if start > 0 else 0
+        end = m.end()+10
+        end = end if end <= text_len else text_len
+        matches.append(text[start:end])
+
+    return matches
+
